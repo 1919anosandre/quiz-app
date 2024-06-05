79c1889ce0e6e32b9215111cb267d61e94706c27
André Luiz <133181905+1919anosandre@users.noreply.github.com>
2024-06-05 19:34:00 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>meu primeiro programa em react</h1>
+    <p>estudar react</p>
+    </div>
+  );
+}
+
+export default App;
